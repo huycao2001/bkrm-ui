6a3c891a7a6c00a3f2bb0b4e352a3ecc8a87c3ef
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-01 01:01:28 +0700

    Install react router, redux nad create a store.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "./store/slice/authSlice";
+
+
 
 
 function App() {
+
+  var isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch()
+  console.log(isLoggedIn);
   return (
     <div className="App">
-      <p>This is the main page</p>
+      
+      <p>{String(isLoggedIn)}</p>
+      <button onClick={() => dispatch(authActions.logIn())}>Login </button>
 
 
     </div>
