4162377b9726039b44394db6c58af8ef0e5e60ae
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-02 08:14:14 +0700

    Add infoSlice to store the state of the user.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slice/authSlice";
 import customizeSlice from "./slice/customizeSlice";
+import infoSlice from "./slice/infoSlice";
 
 
 const store = configureStore({
     reducer : {
         auth :authSlice.reducer,
-        customize: customizeSlice.reducer
+        customize: customizeSlice.reducer,
+        info: infoSlice.reducer,
     },
 })
 
