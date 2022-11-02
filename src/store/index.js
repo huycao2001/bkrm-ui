54806e0ce985d56c4eb50dd75a0999648c2fd254
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-02 08:10:51 +0700

    Add customizaSlice to store the theme state of the app.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slice/authSlice";
+import customizeSlice from "./slice/customizeSlice";
 
 
 const store = configureStore({
     reducer : {
         auth :authSlice.reducer,
+        customize: customizeSlice.reducer
     },
 })
 
