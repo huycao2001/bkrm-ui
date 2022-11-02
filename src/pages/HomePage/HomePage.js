63a00fb6d477ff607f1a4442e6fe5198ff327c30
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-03 00:43:40 +0700

    Add style and create the navbar for home page.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { useLocation, useParams, useRouteMatch } from "react-router-dom";
 
+import { useTheme } from "@material-ui/core/styles";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { AppBar, Toolbar, Typography, AdbIcon } from "@material-ui/core";
+
+// import AdbIcon from '@mui/icons-material/Adb';
+
+import useStyles from "./styles";
+
 const HomePage = (props) => {
-    const path = useRouteMatch(); 
-    console.log(`${path.path}/sales` );
+    const path = useRouteMatch();
+    
+    const customization = useSelector((state) => state.customize);
+    const theme = useTheme();
+    const classes = useStyles(theme);
     console.log("home page called with path " + `/${path.path}`);
     
     return (
-        <div>
-            This is the HomePage
-        </div>
+        <AppBar className={classes.appBar}>
+            <Toolbar>
+            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+                <Typography variant="h3" style={{ marginTop: 15, marginLeft: 20 }}>
+                    BKRM
+                </Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
 
