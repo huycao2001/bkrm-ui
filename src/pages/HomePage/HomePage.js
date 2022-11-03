c8798bf59a5f5c2e7f427f393c39e69fcb4f3528
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-04 01:12:27 +0700

    Add navbar.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useLocation, useParams, useRouteMatch } from "react-router-dom";
 
-import { useTheme } from "@material-ui/core/styles";
+import { useTheme, styled } from "@material-ui/core/styles";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { AppBar, Toolbar, Typography, AdbIcon } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, AdbIcon, Grid, Button } from "@material-ui/core";
+
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 // import AdbIcon from '@mui/icons-material/Adb';
 
@@ -13,21 +15,64 @@ import useStyles from "./styles";
 
 const HomePage = (props) => {
     const path = useRouteMatch();
-    
+
     const customization = useSelector((state) => state.customize);
     const theme = useTheme();
     const classes = useStyles(theme);
-    console.log("home page called with path " + `/${path.path}`);
-    
+    console.log("home page called with path " + `/${path}`);
+    console.log(useLocation());
+    console.log( "DD : "+ classes.appBar)
+
+    const ColorButton = styled(Button)(({ theme }) => ({
+        color: "#ffffff",
+        backgroundColor: "#ff906d",
+        padding : "5px",
+        width: 100,
+        "&:hover": {
+          backgroundColor: "#fa6232",
+        },
+      }));
+
     return (
-        <AppBar className={classes.appBar}>
-            <Toolbar>
-            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
-                <Typography variant="h3" style={{ marginTop: 15, marginLeft: 20 }}>
-                    BKRM
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <Grid container direction="row" alignItems="center" >
+                        <Grid item sm={2}>
+                            <Typography variant="h3" noWrap className={classes.searchEngine}>
+                                BKRM
+                            </Typography>
+                        </Grid>
+
+                        <Grid container item sm={6} direction="row" alignItems="center">
+                            <Grid container item sm={8} direction="row">
+                                <Button >Bán hàng</Button>
+                                <Button >Kho hàng</Button>
+                                <Button >Nhân sự</Button>
+                                <Button >Quản lý</Button>
+                                <Button >Giới thiệu</Button>
+                            </Grid>
+                        </Grid>
+
+                        <Grid
+                            container
+                            item
+                            sm={2}
+                            direction="row"
+                            justifyContent="flex-end"
+                            spacing={1}
+                        >
+
+                            
+                            <Grid item xs={6}><ColorButton> Đăng nhập</ColorButton></Grid>
+                            <Grid item xs={6}><ColorButton> Đăng kí</ColorButton></Grid>
+                        </Grid>
+
+                    </Grid>
+
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
--- src/pages/HomePage/styles.js
+++ src/pages/HomePage/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 
+
+
 const drawerWidth = 240;
 
 export default makeStyles((theme) => ({
