9b5772c0abc623ca24a55eaf92d3610b1713e0ef
unknown <huy.cao2001@hcmut.edu.vn>
2022-11-15 18:54:32 +0700

    Add product api to get products from a branch of a store.

--- src/views/InventoryView/Inventory/Inventory.js
+++ src/views/InventoryView/Inventory/Inventory.js
@@ -7,6 +7,7 @@ import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import productApi from '../../../api/productApi'
 import Category from "./Category/Category";
 import { useState, useEffect, React } from "react";
 import {
@@ -27,11 +28,43 @@ import AddInventory from "./AddInventory/AddInventory";
 
 
 export default function Inventory() {
+    const [productList, setProductList] = useState([]);
+
+    //Redux
     const infoDetail = useSelector((state) => state.info);
+    const store_uuid = infoDetail.store.uuid;
+    const branch_uuid = infoDetail.branch.uuid;
+
     const theme = useTheme();
     const classes = useStyles(theme);
     const xsScreen = useMediaQuery(theme.breakpoints.down("xs"));
     const [openAddInventoryDialog, setOpenAddInventoryDialog] = useState(false);
+    const initialQuery = {
+        orderBy: "products.created_at",
+        sort: "desc",
+        searchKey: "",
+        minStandardPrice: "",
+        maxStandardPrice: "",
+        minListPrice: "",
+        maxListPrice: "",
+        minInventory: "",
+        maxInventory: "",
+        status: "",
+        categoryId: "",
+    };
+    const handleRemoveFilter = () => {
+        setQuery(initialQuery);
+    };
+    const [totalRows, setTotalRows] = useState(0);
+    const [query, setQuery] = useState(initialQuery);
+    const [pagingState, setPagingState] = useState({
+        page: 0,
+        limit: 10,
+    });
+
+
+
+
     const handleClose = () => {
         setOpenAddInventoryDialog(false);
     };
@@ -50,12 +83,47 @@ export default function Inventory() {
         setOpenCategory(false);
     };
 
+
+
+    useEffect(() => {
+        setPagingState({ ...pagingState, page: 0 });
+    }, [reload, store_uuid, branch_uuid, query]);
+
+
+    useEffect(() => {
+        const loadData = async () => {
+          try {
+            const response = await productApi.getProductsOfBranch(
+              store_uuid,
+              branch_uuid,
+              {
+                page: pagingState.page,
+                limit: pagingState.limit,
+                ...query,
+              }
+            );
+            setTotalRows(response.total_rows);
+            setProductList(response.data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        if (store_uuid && branch_uuid) {
+          loadData();
+        }
+      }, [pagingState.page, pagingState.limit, branch_uuid, reload, query]);
+
+
+
+
+
     return (
         <Card className={classes.root} >
             <Grid container direction="row" justifyContent="space-between">
                 <Typography className={classes.headerTitle} variant="h5" >
-                    Sản phẩm 
+                    Sản phẩm
                 </Typography>
+
                 <Grid>
                     <Tooltip title="Thiết lập danh mục">
                         <Button
@@ -63,7 +131,7 @@ export default function Inventory() {
                             color="primary"
                             className={classes.button}
                             startIcon={<FileCopyIcon />}
-                            onClick = {handleClickOpenCategory}
+                            onClick={handleClickOpenCategory}
                         >
                             Danh mục
                         </Button>
@@ -104,6 +172,8 @@ export default function Inventory() {
                 </Grid>
 
             </Grid>
+
+
             <AddInventory
                 open={openAddInventoryDialog}
                 handleClose={handleClose}
