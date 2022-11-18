4ad4c7ba2666e12856ee65a29027baa24b0452e9
Raverbury <smurfjp198@gmail.com>
2022-11-18 13:07:46 +0700

    403 handler

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -33,11 +33,19 @@ axiosClient.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response?.status == "401"  ) {
+    console.log("In client: " + error.message);
+    if (error.response?.status === "401"  ) {
       if (!(document.URL.includes("/login") || document.URL.includes("/signup") )) {
-        console.log(!document.URL.includes("/login"))
-        store.dispatch(authActions.logOut())
-        store.dispatch(statusAction.failedStatus("Hết phiên đăng nhập"))
+        console.log(!document.URL.includes("/login"));
+        store.dispatch(authActions.logOut());
+        store.dispatch(statusAction.failedStatus("Hết phiên đăng nhập"));
+      }
+    }
+    if (error.response?.status === "403"  ) {
+      if (!(document.URL.includes("/login") || document.URL.includes("/signup") )) {
+        console.log(!document.URL.includes("/login"));
+        store.dispatch(authActions.logOut());
+        store.dispatch(statusAction.failedStatus("Please get an administrator to approve your account"));
       }
     }
     throw error
