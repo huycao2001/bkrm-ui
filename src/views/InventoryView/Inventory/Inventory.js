7695d0b57b6b77c66e0d32ac1e555947f428a703
Chuong Nguyen <chuongnt9@gmail.com>
2022-11-28 22:31:21 +0700

    update inventory ui

--- src/views/InventoryView/Inventory/Inventory.js
+++ src/views/InventoryView/Inventory/Inventory.js
@@ -1,17 +1,17 @@
-
-// import React from react; 
+// import React from react;
 
 import { useDispatch, useSelector } from "react-redux";
 import useStyles from "../../../components/TableCommon/style/mainViewStyle";
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import productApi from '../../../api/productApi'
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import productApi from "../../../api/productApi";
+import storeApi from "../../../api/storeApi";
 import Category from "./Category/Category";
 import SnackBar from "../../../components/SnackBar/SnackBar";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Typography,
   Card,
@@ -22,34 +22,89 @@ import {
   Avatar,
   Tooltip,
   TableBody,
-  Box
+  Box,
 } from "@material-ui/core";
+import { useReactToPrint } from "react-to-print";
 
-import AddIcon from '@mui/icons-material/Add';
+import { trackPromise } from "react-promise-tracker";
+import AddIcon from "@mui/icons-material/Add";
 import AddInventory from "./AddInventory/AddInventory";
 
-// Components 
+// Constants
+import * as TableType from "../../../assets/constant/tableType";
+import * as HeadCells from "../../../assets/constant/tableHead";
+// Components
 import TableWrapper from "../../../components/TableCommon/TableWrapper/TableWrapper";
+import TableHeader from "../../../components/TableCommon/TableHeader/TableHeader";
 import InventoryTableRow from "./InventoryTableRow/InventoryTableRow";
+import LoadingIndicator from "../../../components/LoadingIndicator/LoadingIndicator";
+import ToolBar from "../../../components/TableCommon/ToolBar/ToolBar";
 
 //Setting
-import setting from "../../../assets/constant/setting"
+import setting from "../../../assets/constant/setting";
 
-
+import { statusAction } from "../../../store/slice/statusSlice";
+import CustomExcel from "./CustomExcel";
+import * as excel from "../../../assets/constant/excel";
 export default function Inventory() {
   const [productList, setProductList] = useState([]);
-
+  const [searchValue, setSearchValue] = useState("");
+  const dispatch = useDispatch();
   //Redux
   const infoDetail = useSelector((state) => state.info);
   const store_uuid = infoDetail.store.uuid;
   const branch_uuid = infoDetail.branch.uuid;
-  const store_setting = infoDetail.store.general_configuration? JSON.parse(infoDetail.store.general_configuration): setting
+  const store_setting = infoDetail.store.general_configuration
+    ? JSON.parse(infoDetail.store.general_configuration)
+    : setting;
+
+  const [openFilter, setOpenFilter] = React.useState(false);
+  const handleToggleFilter = () => {
+    setOpenFilter(!openFilter);
+  };
+  const importProductByJSON = async (jsonData) => {
+    try {
+      setOpenProductImportPopper(true);
+      setIsLoadingProduct(true);
+      const res = await storeApi.importProductJSON(
+        store_uuid,
+        branch_uuid,
+        jsonData
+      );
+      if (res.status) {
+        setIsLoadingProduct(false);
+        setOpenProductImportPopper(false);
+        setReload(!reload);
+        dispatch(statusAction.successfulStatus("Nhập hàng thành công"));
+      } else {
+        setIsLoadingProduct(false);
+        setProductErrors(res.data);
+        dispatch(statusAction.failedStatus("Nhập hàng thất bại"));
+      }
+      // setOpen(true);
+    } catch (err) {
+      console.log(err);
+      dispatch(statusAction.failedStatus("Nhập hàng thất bại"));
+      setIsLoadingProduct(false);
+      // setOpen(false);
+    }
+  };
+
+  // header sort
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("id");
+
+  const handleRequestSort = (event, property) => {
+    //// (gửi order vs orderBy lên api) -> fetch lại data để sort
+    // const isAsc = orderBy === property && order === 'asc';
+    // setOrder(isAsc ? 'desc' : 'asc');
+    // setOrderBy(property);
+  };
 
   // Styling
   const theme = useTheme();
   const classes = useStyles(theme);
   const xsScreen = useMediaQuery(theme.breakpoints.down("xs"));
-
 
   const [openAddInventoryDialog, setOpenAddInventoryDialog] = useState(false);
   const [openBar, setOpenBar] = React.useState(false);
@@ -80,19 +135,15 @@ export default function Inventory() {
     limit: 10,
   });
 
-
-
-    //Collapse the row
-    const [openRow, setRowOpen] = React.useState(null);
-    const handleOpenRow = (row) => {
-      if (row !== openRow) {
-        setRowOpen(row);
-      } else {
-        setRowOpen(null);
-      }
-    };
-
-
+  //Collapse the row
+  const [openRow, setRowOpen] = React.useState(null);
+  const handleOpenRow = (row) => {
+    if (row !== openRow) {
+      setRowOpen(row);
+    } else {
+      setRowOpen(null);
+    }
+  };
 
   const handleClose = () => {
     setOpenAddInventoryDialog(false);
@@ -101,7 +152,6 @@ export default function Inventory() {
   const handleClickOpen = () => {
     setOpenAddInventoryDialog(true);
   };
-
 
   // Category
   const [openCategory, setOpenCategory] = useState(false);
@@ -112,24 +162,19 @@ export default function Inventory() {
     setOpenCategory(false);
   };
 
-
-
   useEffect(() => {
     setPagingState({ ...pagingState, page: 0 });
   }, [reload, store_uuid, branch_uuid, query]);
 
-
   useEffect(() => {
     const loadData = async () => {
       try {
-        const response = await productApi.getProductsOfBranch(
-          store_uuid,
-          branch_uuid,
-          {
+        const response = await trackPromise(
+          productApi.getProductsOfBranch(store_uuid, branch_uuid, {
             page: pagingState.page,
             limit: pagingState.limit,
             ...query,
-          }
+          })
         );
         setTotalRows(response.total_rows);
         setProductList(response.data);
@@ -142,14 +187,21 @@ export default function Inventory() {
     }
   }, [pagingState.page, pagingState.limit, branch_uuid, reload, query]);
 
-
-
-
   //const tableRef = React.createRef();
+  // toolbar
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  /// import product by file
+  const [openProductImportPopper, setOpenProductImportPopper] = useState(false);
+  const [isLoadingProduct, setIsLoadingProduct] = useState(false);
+  const [productErrors, setProductErrors] = useState([]);
   return (
-    <Card className={classes.root} >
+    <Card className={classes.root}>
       <Grid container direction="row" justifyContent="space-between">
-        <Typography className={classes.headerTitle} variant="h5" >
+        <Typography className={classes.headerTitle} variant="h5">
           Sản phẩm
         </Typography>
 
@@ -159,7 +211,6 @@ export default function Inventory() {
               variant="outlined"
               color="primary"
               className={classes.button}
-
               startIcon={<FileCopyIcon />}
               onClick={handleClickOpenCategory}
             >
@@ -171,7 +222,6 @@ export default function Inventory() {
             <Button
               variant="outlined"
               color="primary"
-
               startIcon={<AddIcon />}
               onClick={handleClickOpen}
             >
@@ -179,17 +229,8 @@ export default function Inventory() {
             </Button>
           </Tooltip>
 
-
-
           <Category open={openCategory} handleClose={handleCloseCategory} />
-
-
-          
-
-
-
         </Grid>
-
       </Grid>
       <Category open={openCategory} handleClose={handleCloseCategory} />
       {openAddInventoryDialog && (
@@ -205,30 +246,91 @@ export default function Inventory() {
         handleCloseBar={handleCloseBar}
         addStatus={addStatus}
       />
+      <ToolBar
+        textSearch={"Mã , Tên sản phẩm,..."}
+        dataTable={productList}
+        tableType={TableType.INVENTORY}
+        handlePrint={handlePrint}
+        handleSearchValueChange={setSearchValue}
+        handleToggleFilter={handleToggleFilter}
+        hasImport={true}
+        importByJSON={importProductByJSON}
+        excel_data={excel.excel_data_product}
+        excel_name={excel.excel_name_product}
+        customizable={true}
+        customExcel={CustomExcel}
+        columnsToKeep={[
+          { dbName: "product_code", displayName: "Mã sản phẩm" },
+          { dbName: "name", displayName: "Sản phẩm" },
+          { dbName: "bar_code", displayName: "Mã vạch" },
+          { dbName: "list_price", displayName: "Giá bán" },
+          { dbName: "standard_price", displayName: "Giá vốn" },
+          { dbName: "quantity_per_unit", displayName: "Đơn vị" },
+          { dbName: "category_id", displayName: "Danh mục" },
+          { dbName: "img_urls", displayName: "Hình ảnh (url1, url2,...)" },
+          { dbName: "quantity_available", displayName: "Tồn kho" },
+          { dbName: "min_reorder_quantity", displayName: "Tồn nhỏ nhất" },
+          { dbName: "max_order", displayName: "Tồn lớn nhất" },
+          { dbName: "has_batches", displayName: "Lô" },
+          { dbName: "desciption", displayName: "Mô tả" },
+        ]}
+        orderByOptions={
+          store_setting?.inventory.status
+            ? [
+                { value: "products.created_at", label: "Ngày tạo" },
+                { value: "products.list_price", label: "Gia ban" },
+                { value: "products.standard_price", label: "Gia von" },
+                { value: "products.quantity_available", label: "Ton kho" },
+              ]
+            : [
+                { value: "products.created_at", label: "Ngày tạo" },
+                { value: "products.list_price", label: "Gia ban" },
+                { value: "products.standard_price", label: "Gia von" },
+              ]
+        }
+        orderBy={query.orderBy}
+        setOrderBy={(value) => setQuery({ ...query, orderBy: value })}
+        sort={query.sort}
+        setSort={(value) => setQuery({ ...query, sort: value })}
+        searchKey={query.searchKey}
+        setSearchKey={(value) => setQuery({ ...query, searchKey: value })}
+        handleRemoveFilter={handleRemoveFilter}
+      />
+      <LoadingIndicator />
       <TableWrapper
-            pagingState={{ ...pagingState, total_rows: totalRows }}
-            setPagingState={setPagingState}
-            list={productList}
-            tableRef={null}
-          >
-
-
-            <TableBody>
-              {productList.map((row, index) => {
-                return (
-                  <InventoryTableRow
-                    key={row.uuid}
-                    row={row}
-                    setReload={() => setReload(!reload)}
-                    openRow={openRow}
-                    handleOpenRow={handleOpenRow}
-                    isManageInventory={store_setting?.inventory.status}
-                  />
-                );
-              })}
-
-            </TableBody>
-          </TableWrapper>
+        pagingState={{ ...pagingState, total_rows: totalRows }}
+        setPagingState={setPagingState}
+        list={productList}
+        tableRef={null}
+      >
+        <TableHeader
+          classes={classes}
+          order={order}
+          orderBy={orderBy}
+          onRequestSort={handleRequestSort}
+          headerData={
+            store_setting?.inventory.status
+              ? HeadCells.InventoryHeadCells
+              : HeadCells.InventoryHeadCells.filter(
+                  (item) => item.id !== "inventory" && item.id !== "quantity"
+                )
+          }
+        />
+        <TableBody>
+          {productList.map((row, index) => {
+            return (
+              <InventoryTableRow
+                key={row.uuid}
+                row={row}
+                setReload={() => setReload(!reload)}
+                openRow={openRow}
+                handleOpenRow={handleOpenRow}
+                isManageInventory={store_setting?.inventory.status}
+              />
+            );
+          })}
+        </TableBody>
+      </TableWrapper>
     </Card>
   );
-};
+}
