27a3ab3443f334f8b50045e295cf49668c5149d6
unknown <huy.cao2001@hcmut.edu.vn>
2022-12-06 23:20:26 +0700

    Add invoide view for sales

--- src/views/SalesView/SalesView.js
+++ src/views/SalesView/SalesView.js
@@ -1,6 +1,6 @@
 import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 import Cart from "./Cart/Cart";
-// import Invoice from "./Invoice/Invoice";
+import Invoice from "./Invoice/Invoice";
 // import InvoiceReturn from "./InvoiceReturn/InvoiceReturn";
 //import OrderProductList from "../InventoryView/OrderProductList/OrderProductList";
 import React from "react";
@@ -21,8 +21,8 @@ const SalesView = (props) => {
         />
       </Route>
       <Route exact path={`${path}/cart`} component={Cart} />
-      {/* <Route path={`${path}/invoice`} component={Invoice} />
-      <Route path={`${path}/invoice-return`} component={InvoiceReturn} /> */}
+      <Route path={`${path}/invoice`} component={Invoice} />
+      {/*<Route path={`${path}/invoice-return`} component={InvoiceReturn} /> */}
       {/* <Route path={`${path}/order-list/order`} component={OrderProduct} /> */}
       {/* <Route path={`${path}/order-list`} component={OrderProductList} /> */}
     </Switch>
