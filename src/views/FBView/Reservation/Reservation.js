958fc8f6c7e5f5fa2ef9a2dc3e7116b0fbbb586f
Chuong Nguyen <chuongnt9@gmail.com>
2023-02-18 14:39:40 +0700

    only show reserved tables in reservation

--- src/views/FBView/Reservation/Reservation.js
+++ src/views/FBView/Reservation/Reservation.js
@@ -25,35 +25,42 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { blue, orange } from '@mui/material/colors';
 
-import { data as appointments } from './demo-data/grouping';
-
 import AddReservation from "./AddReservation/AddReservation";
 import fbReservationApi from "../../../api/fbReservationApi";
-const resources = [{
+import LoadingIndicator from "../../../components/LoadingIndicator/LoadingIndicator";
+const sampleResources = [{
     fieldName: 'tableId',
     title: 'Table',
     instances: [
         { text: 'Table 1', id: 1, color: blue },
-        { text: 'Table 2', id: 2, color: orange },
-        { text: 'Table 3', id: 3, color: blue },
-        { text: 'Table 4', id: 4, color: orange },
-        { text: 'Table 5', id: 5, color: blue },
-        { text: 'Table 6', id: 6, color: orange },
-        { text: 'Table 7', id: 7, color: blue },
-        { text: 'Table 8', id: 8, color: orange },
-        { text: 'Table 9', id: 9, color: blue },
-        { text: 'Table 10', id: 10, color: orange },
-        { text: 'Table 11', id: 11, color: blue },
-        { text: 'Table 12', id: 12, color: orange },
-        { text: 'Table 13', id: 13, color: blue },
-        { text: 'Table 14', id: 14, color: orange },
-        { text: 'Table 15', id: 15, color: blue },
-        { text: 'Table 16', id: 16, color: orange },
-        { text: 'Table 17', id: 17, color: blue },
-        { text: 'Table 18', id: 18, color: orange },
-        { text: 'Table 19', id: 19, color: blue },
-        { text: 'Table 20', id: 20, color: orange },
+        // { text: 'Table 2', id: 2, color: orange },
+        // { text: 'Table 3', id: 3, color: blue },
+        // { text: 'Table 4', id: 4, color: orange },
+        // { text: 'Table 5', id: 5, color: blue },
+        // { text: 'Table 6', id: 6, color: orange },
+        // { text: 'Table 7', id: 7, color: blue },
+        // { text: 'Table 8', id: 8, color: orange },
+        // { text: 'Table 9', id: 9, color: blue },
+        // { text: 'Table 10', id: 10, color: orange },
+        // { text: 'Table 11', id: 11, color: blue },
+        // { text: 'Table 12', id: 12, color: orange },
+        // { text: 'Table 13', id: 13, color: blue },
+        // { text: 'Table 14', id: 14, color: orange },
+        // { text: 'Table 15', id: 15, color: blue },
+        // { text: 'Table 16', id: 16, color: orange },
+        // { text: 'Table 17', id: 17, color: blue },
+        // { text: 'Table 18', id: 18, color: orange },
+        // { text: 'Table 19', id: 19, color: blue },
+        // { text: 'Table 20', id: 20, color: orange },
     ],
+}];
+const testData = [{
+    title: 'Website Re-Design Plan',
+    tableId: 11,
+    startDate: new Date("2023-02-17 9:00:00"),
+    // startDate: new Date("2023-02-17 21:00:00"),
+    endDate: new Date("2023-02-17 21:00:00"),
+    id: 0,
 }];
 const grouping = [{
     resourceName: 'tableId',
@@ -64,16 +71,34 @@ Date.prototype.addHours = function (h) {
 }
 
 export default () => {
-
+    const [resources, setResources] = React.useState([{
+        fieldName: 'tableId',
+        title: 'Table',
+        instances: [
+            { text: 'Table 1', id: 1, color: blue },
+            // { text: 'Table 2', id: 2, color: orange },
+            // { text: 'Table 3', id: 3, color: blue },
+            // { text: 'Table 4', id: 4, color: orange },
+            // { text: 'Table 5', id: 5, color: blue },
+            // { text: 'Table 6', id: 6, color: orange },
+            // { text: 'Table 7', id: 7, color: blue },
+            // { text: 'Table 8', id: 8, color: orange },
+            // { text: 'Table 9', id: 9, color: blue },
+            // { text: 'Table 10', id: 10, color: orange },
+            // { text: 'Table 11', id: 11, color: blue },
+            // { text: 'Table 12', id: 12, color: orange },
+            // { text: 'Table 13', id: 13, color: blue },
+            // { text: 'Table 14', id: 14, color: orange },
+            // { text: 'Table 15', id: 15, color: blue },
+            // { text: 'Table 16', id: 16, color: orange },
+            // { text: 'Table 17', id: 17, color: blue },
+            // { text: 'Table 18', id: 18, color: orange },
+            // { text: 'Table 19', id: 19, color: blue },
+            // { text: 'Table 20', id: 20, color: orange },
+        ],
+    }]);
     // const [reservationList, setReservationList] = useState([]);
-    const testData = [{
-        title: 'Website Re-Design Plan',
-        tableId: 11,
-        startDate: new Date("2023-02-17 21:00:00"),
-        // startDate: new Date("2023-02-17 21:00:00"),
-        endDate: new Date("2023-02-17 21:00:00"),
-        id: 0,
-    }]
+
 
     const [data, setData] = React.useState(testData);
     const onCommitChanges = React.useCallback(({ added, changed, deleted }) => {
@@ -102,6 +127,7 @@ export default () => {
     const store_uuid = infoDetail.store.uuid;
     const branch_uuid = infoDetail.branch.uuid;
 
+    const [currentDate, setCurrentDate] = useState(new Date().toISOString().slice(0, 11).replace('T', ' '));
     useEffect(() => {
         const loadReservations = async () => {
             try {
@@ -110,30 +136,38 @@ export default () => {
                         store_uuid,
                         branch_uuid,
                         {
-                            //   ...query,
-                            //   ...pagingState
+                            currentDate,
                         }
                     )
                 );
                 // setReservationList(response.data.reservations);
-                console.log("zzzzzzzz");
-                console.log(response.data.reservations);
-                console.log("this is reservation list: ");
-                // console.log(reservationList);
-                var allReservations = response.data.reservations.map(function (item) {
-                    return {
-                        title: item.name,
-                        tableId: item.table.id,
-                        startDate: new Date(item.reservation_datetime),
-                        // startDate: new Date("2023-02-17 21:00:00"),
-                        endDate: new Date(item.reservation_endtime),
-                        id: item.id,
-                    };
-                });
-                // console.log("KOKO");
-                // console.log(allReservations);
-                // console.log(appointments);
-                setData(allReservations);
+                // console.log("zzzzzzzz");
+                // console.log(response.data.reservations);
+                // console.log("this is reservation list: ");
+                if (response.data.reservations.length > 0) {
+                    setData(response.data.reservations.map(function (item) {
+                        return {
+                            title: item.name,
+                            tableId: item.table.id,
+                            startDate: new Date(item.reservation_datetime),
+                            endDate: new Date(item.reservation_endtime),
+                            id: item.id,
+                        };
+                    }));
+                    setResources([{
+                        fieldName: 'tableId',
+                        title: 'Table',
+                        instances: response.data.reservations.map(function (item) {
+                            return { text: 'Table ' + item.id.toString(), id: item.table.id, color: blue };
+                        }),
+                    }]);
+                }
+                else {
+                    console.log("there is no reservations");
+                    setResources(sampleResources);
+                    setData(testData);
+                }
+
             } catch (error) {
                 console.log(error);
                 console.log("FAILED");
@@ -142,11 +176,12 @@ export default () => {
         if (store_uuid && branch_uuid) {
             loadReservations();
         }
-    }, [branch_uuid, reload]);
+    }, [branch_uuid, reload, currentDate]);
 
 
     return (
         <Paper>
+
             <Tooltip title="Thêm bàn mới">
                 <Button
                     variant="outlined"
@@ -170,7 +205,12 @@ export default () => {
                 data={data}
                 height={660}
             >
+
                 <ViewState
+                    onCurrentDateChange={(e) => {
+                        console.log("date" + JSON.stringify(e).slice(1, 11));
+                        setCurrentDate(JSON.stringify(e).slice(1, 11));
+                    }}
                 />
                 <EditingState
                     onCommitChanges={onCommitChanges}
